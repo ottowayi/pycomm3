37f88ba9c802b679617adc4b7f903ae696da299c
ruscito <ruscito@gmail.com>
2014-12-26 00:58:17 -0600

    Add internal socket class plus some support functions. Implemented nop  register session and partially list identity

--- eip.py
+++ eip.py
@@ -1,19 +1,133 @@
+__version__ = "$Revision$"
+# $Source$
+
 import socket
 import struct
+from multiprocessing import Process, Event
+from time import sleep
+
+COMMAND = {"nop": 0x00,
+               "list_targets": 0x01,
+               "list_services": 0x04,
+               "list_identity": 0x63,
+               "list_interfaces": 0x64,
+               "register_session": 0x65,
+               "unregister_session": 0x66,
+               "send_rr_data": 0x6F,
+               "send_unit_data": 0x70}
+
+STATUS = {0x0000: "0x0000: Success",
+          0x0001: "0x0001: Unsupported Command",
+          0x0002: "0x0002: No Resources to Process",
+          0x0003: "0x0003: Poorly Formed/Bad Data Attached",
+          0x0064: "0x0064: Invalid Session",
+          0x0065: "0x0065: Request was Invalid Length",
+          0x0069: "0x0069: Unsupported Protocol Version"}
+
+HEADER_SIZE = 24
 
 
-class EipBase:
+def pack_uint(n):
+    """pack 16 bit into 2 bytes little indian"""
+    return struct.pack('<H', n)
+
+
+def pack_dint(n):
+    """pack 32 bit into 4 bytes little indian"""
+    return struct.pack('<I', n)
+
+
+def unpack_uint(st):
+    """unpack 2 bytes little indian to int"""
+    return int(struct.unpack('<H', st[0:2])[0])
+
+
+def unpack_dint(st):
+    """unpack 4 bytes little indian to int"""
+    return int(struct.unpack('<I', st[0:4])[0])
+
+
+def print_info(msg):
+    """
+    nice formatted print for the encapsulated message
+    :param msg: the encapsulated message to print
+    :return:
+    """
+    print
+    n = len(msg)
+    print "  Full length of EIP = %d (0x%04x)" % (n, n)
+
+    cmd = unpack_uint(msg[:2])
+    print "         EIP Command =",
+    if cmd == 0:
+        print "NOP"
+    elif cmd == 0x01:
+        print "List Targets"
+    elif cmd == 0x04:
+        print "List Services"
+    elif cmd == 0x63:
+        print "List Identity"
+    elif cmd == 0x64:
+        print "List Interfaces"
+    elif cmd == 0x65:
+        print "Register Session"
+    elif cmd == 0x66:
+        print "Unregister Session"
+    else:
+        print "Unknown command: 0x%02x" % cmd
+
+    # The Data Part
+    d = unpack_uint(msg[2:4])
+    print "Attached Data Length = %d" % d
+
+    n = unpack_dint(msg[4:8])
+    print "      Session Handle = %d (0x%08x)" % (n, n)
+
+    n = unpack_dint(msg[8:12])
+    print "      Session Status = %d (0x%08x)" % (n, n)
+
+    print "      Sender Context = %s" % msg[12:20]
+
+    n = unpack_dint(msg[20:24])
+    print "    Protocol Options = %d (0x%08x)" % (n, n)
+
+    if 0 < d < 500:
+        print "data =", list(msg[24:])
+    elif d > 500:
+        print "attached data is longer than 500 bytes"
+    print
+
+    return msg
+
+
+def print_bytes(msg):
+    print '[%d]\n' % len(msg)
+    for ch in msg:
+        print ("%02X" % ord(ch))
+    return
+
+
+class Eip:
     class ConnectionError(Exception):
         pass
 
-    class EipSocket:
-        def __init__(self, sock):
-            if sock is None:
-                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    class Socket:
+        def __init__(self, timeout):
+            self.timeout = 5.0
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if timeout is None:
+                self.sock.settimeout(self.timeout)
             else:
-                self.sock = sock
-            self.sock.settimeout(5.0)
+                self.sock.settimeout(timeout)
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+
+        @property
+        def timeout(self):
+            return self.timeout
+
+        @timeout.setter
+        def timeout(self, par):
+            self.timeout = par
 
         def connect(self, host, port):
             try:
@@ -28,52 +142,53 @@ class EipBase:
                 if sent == 0:
                     raise RuntimeError("socket connection broken")
                 total_sent += sent
+            return total_sent
 
-    def receive(self, msg_len):
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < msg_len:
-            chunk = self.sock.recv(min(msg_len - bytes_recd, 2048))
-            if chunk == '':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd += len(chunk)
-        return ''.join(chunks)
+        def receive(self):
+            msg_len = 28
+            chunks = []
+            bytes_recd = 0
+            one_shot = True
+            while bytes_recd < msg_len:
+                chunk = self.sock.recv(min(msg_len - bytes_recd, 2048))
+                if chunk == '':
+                    raise RuntimeError("socket connection broken")
 
-    EIP_COMMAND = {"nop": 0x00,
-                   "list_targets": 0x01,
-                   "list_services": 0x04,
-                   "list_identity": 0x63,
-                   "list_interfaces": 0x64,
-                   "register_session": 0x65,
-                   "unregister_session": 0x66,
-                   "send_rr_data": 0x6F,
-                   "send_unit_data": 0x70}
+                if one_shot:
+                    msg_len = HEADER_SIZE + int(struct.unpack('<H', chunk[2:4])[0])
+                    if msg_len == 0:
+                        msg_len = HEADER_SIZE + int(struct.unpack('<H', chunk[24:28])[0])
+                    print "Size of received msg %d" % msg_len
+                    one_shot = False
 
-    EIP_STATUS = {0x0000: "0x0000: Success",
-                  0x0001: "0x0001: Unsupported Command",
-                  0x0002: "0x0002: No Resources to Process",
-                  0x0003: "0x0003: Poorly Formed/Bad Data Attached",
-                  0x0064: "0x0064: Invalid Session",
-                  0x0065: "0x0065: Request was Invalid Length",
-                  0x0069: "0x0069: Unsupported Protocol Version"}
+                chunks.append(chunk)
+                bytes_recd += len(chunk)
+            return ''.join(chunks)
+
+        def close(self):
+            self.sock.close()
 
     def __init__(self):
         self.__version__ = '0.1'
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s = self.EipSocket(None)
-
-        self.params = {'session': 0, 'status': 0, 'name': 'ucmm',
-                        'port': 0xAF12, 'timeout': 5.0, 'context': '_pycomm_',
-                        'option': 0, 'protocol_version': 0}
+        self.__sock = self.Socket(None)
         self.session = 0
         self.context = '_pycomm_'
-        self.protocol_version = 0
+        self.protocol_version = 1
         self.status = 0
         self.name = 'ucmm'
-        self.port = 0xAF12
-        self.timeout = 5.0
         self.option = 0
+        self.port = 0xAF12
+        self.session_registered = False
+        self.connection_opened = False
+
+
+    @property
+    def port(self):
+        return self.port
+
+    @port.setter
+    def port(self, par):
+        self.port = par
 
     @property
     def session(self):
@@ -100,172 +215,115 @@ class EipBase:
     def status(self, par):
         self.status = par
 
-    def set_session(self, s):
-        self.params['session'] = s
+    @property
+    def name(self):
+        return self.port
 
-    def version(self):
-        return self.__version__
+    @name.setter
+    def name(self, par):
+        self.name = par
 
-    def str_AddLE16(self, n): # convert 16-bit integer into 2xbyte
-        return struct.pack('<H', n)
-
-    def str_AddLE32( self, n): # convert 32-bit integer into 4xbyte
-        return struct.pack('<I', n)
-
-    def n_ParseLE16( self, st): # convert 16-bit integer into 2xbyte
-        return int(struct.unpack('<H', st[0:2])[0])
-
-    def n_ParseLE32( self, st): # convert 32-bit integer into 4xbyte
-        return int(struct.unpack('<I', st[0:4])[0])
-
-    def print_info(self, eip_hdr):
-        print
-        n = len( eip_hdr)
-        print "  Full length of EIP = %d (0x%04x)" % (n,n)
-
-        cmd = self.n_ParseLE16(eip_hdr[:2])
-        print "         EIP Command =",
-        if cmd == 0:
-            print "NOP"
-        elif cmd == 0x01:
-            print "List Targets"
-        elif cmd == 0x04:
-            print "List Services"
-        elif cmd == 0x63:
-            print "List Identity"
-        elif cmd == 0x64:
-            print "List Interfaces"
-        elif cmd == 0x65:
-            print "Register Session"
-        elif cmd == 0x66:
-            print "Unregister Session"
+    def returned_error(self, rsp):
+        self.status = unpack_dint(rsp[8:12])
+        if self.status == 0x0000:
+            return False
+        elif self.status == 0x0001:
+            error = STATUS[0x0001]
+        elif self.status == 0x0002:
+            error = STATUS[0x0002]
+        elif self.status == 0x0003:
+            error = STATUS[0x0003]
+        elif self.status == 0x0064:
+            error = STATUS[0x0064]
+        elif self.status == 0x0065:
+            error = STATUS[0x0065]
+        elif self.status == 0x0069:
+            error = STATUS[0x0069]
         else:
-            print "Unknown command: 0x%02x" % cmd
-
-        # The Data Part
-        d = self.n_ParseLE16(eip_hdr[2:4])
-        print "Attached Data Length = %d" % d
-
-        n = self.n_ParseLE32(eip_hdr[4:8])
-        print "      Session Handle = %d (0x%08x)" % (n, n)
-
-        n = self.n_ParseLE32(eip_hdr[8:12])
-        print "      Session Status = %d (0x%08x)" % (n, n)
-
-        print "      Sender Context = %s" % eip_hdr[12:20]
-
-        n = self.n_ParseLE32(eip_hdr[20:24])
-        print "    Protocol Options = %d (0x%08x)" % (n, n)
-
-        if 0 < d < 500:
-            print "data =", list(eip_hdr[24:])
-        elif d > 500:
-            print "attached data is longer than 500 bytes"
-
-        return
-
-    def parse_register_session_reply(self, rsp):
-
-        if not rsp or len(rsp) != 28:
-            print "bad length!"
-            self.set_session(0)
-            return False
-
-        if self.n_ParseLE16(rsp[:2]) != 0x65:
-            print "bad command!"
-            self.set_session(0)
-            return False
-
-        if(self.n_ParseLE32(rsp[8:12])!= 0):
-            print "bad status!"
-            self.set_session(0)
-            return False
-
-        # ignore the rest
-
-        self.session = self.n_ParseLE32(rsp[4:8])
-
-        n = self.session
-        print "EIPC: New Session Handle = %d (0x%08x)" % (n,n)
+            error = "Unrecognized error %d (0x%08x)" % (self.status, self.status)
+        print "Returned %s" % error
         return True
 
     def parse_replay(self, rsp):
-        pass
+        if self.returned_error(rsp):
+            return False
 
-    def __build_header(self, command, lenght):
+        # Get Command
+        command = unpack_uint(rsp[:2])
+        print "Command %d (0x%02x)" % (command, command)
+
+        if command == COMMAND['register_session']:
+            self.session = unpack_dint(rsp[4:8])
+            print "New Session Handle = %d (0x%04x)" % (self.session, self.session)
+            self.session_registered = True
+        elif command == COMMAND['list_identity']:
+            print "item count %d" % unpack_uint(rsp[24:28])
+
+            print_bytes(rsp[28:])
+        else:
+            print "Command %d (0x%02x) unknown or not implemented" % (command, command)
+            return False
+
+        return True
+
+    def build_header(self, command, length):
         """
-        private  method called by the commands method to build the header
-        :param command:
-        :param lenght:
-        :return:header
+        build the encapsulated message header which is a 24 bytes fixed length.
+        The header includes the command and the length of the optional data portion
         """
-        h = self.str_AddLE16(command)
-        h += self.str_AddLE16(lenght)
-        h += self.str_AddLE32(self.session)
-        h += self.str_AddLE32(self.status)
-        h += self.context
-        h += self.str_AddLE32(self.protocol_version)
-        self.print_info(h)
+        h = pack_uint(command)          # Command UINT
+        h += pack_uint(length)          # Length UINT
+        h += pack_dint(self.session)    # Session Handle UDINT
+        h += pack_dint(self.status)     # Status UDINT
+        h += self.context               # Sender Context 8 bytes
+        h += pack_dint(self.option)     # Option UDINT
         return h
 
+    def nop(self):
+        msg = self.build_header(COMMAND['nop'], 0)
+        self.__sock.send(msg)
+
+    def list_identity(self):
+        msg = self.build_header(COMMAND['list_identity'], 0)
+        print_info(msg)
+        self.send(msg)
+        # parse the response
+        self.parse_replay(self.__sock.receive())
+
     def register_session(self):
-        """
-        0x65
-        :return:
-        """
-        msg = self.__build_header(self.EIP_COMMAND['register_session'], 4)
-        msg += self.str_AddLE16(1)
-        msg += self.str_AddLE16(0)
-        self.print_info(msg)
-        return msg
+        msg = self.build_header(COMMAND['register_session'], 4)
+        msg += pack_uint(self.protocol_version)
+        msg += pack_uint(0)
+        self.__sock.send(msg)
+
+        # parse the response
+        self.parse_replay(self.__sock.receive())
+
+        return self.session
 
     def unregister_session(self):
-        return self.__build_header(self.EIP_COMMAND['unregister_session'], 0)
+        msg = self.build_header(COMMAND['unregister_session'], 0)
+        self.__sock.send(msg)
 
     def send(self, msg):
-        if self.sock is not None:
-            self.sock.send(msg)
+        return self.__sock.send(msg)
 
-    def receive(self):
-        pass
+    def receive(self, msg):
+        return self.__sock.receive(msg)
 
     def open(self, ip_address):
         # handle the socket layer
-        self.sock.settimeout(self.params['timeout'])
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-        self.sock.connect_ex((ip_address, self.params['port']))
-
-        self.sock.send(self.register_session())
-
-        # wait for the response
-        rsp = self.sock.recv(100)
-
-        # parse the response
-        self.parse_register_session_reply(rsp)
-
-        # return the session
-        return self.session
+        if not self.connection_opened:
+            self.__sock.connect(ip_address, self.port)
+            self.connection_opened = True
+            return True
+        return False
 
     def close(self):
         if self.session != 0:
-            self.sock.send(self.unregister_session())
+            self.unregister_session()
 
-        self.sock.close()
-        self.sock = None
+        self.__sock.close()
+        self.__sock = None
         self.session = 0
-
-
-    def t_open(self, ip_address):
-        # handle the socket layer
-        self.s.connect(ip_address, self.params['port'])
-
-        self.s.send(self.register_session())
-
-        # wait for the response
-        rsp = self.s.receive(28)
-
-        # parse the response
-        self.parse_register_session_reply(rsp)
-
-        # return the session
-        return self.session
+        self.connection_opened = False
--- pycomm.py
+++ pycomm.py
@@ -1,10 +1,17 @@
-from eip import EipBase
+from eip import Eip
+from time import sleep
 
 if __name__ == '__main__':
-    c = EipBase()
-    # c.open('192.168.1.10')
-    c.t_open('192.168.1.10')
+    c = Eip()
+
+    c.open('192.168.1.10')
+    c.register_session()
+    c.list_identity()
+    run = True
+    while run:
+        try:
+            c.nop()
+            sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            run = False
     c.close()
-
-
-
