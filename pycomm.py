566e77e29fee024a57ce3fdeccbee3897508c048
ruscito <ruscito@gmail.com>
2015-02-23 18:16:37 -0600

    gr

--- pycomm.py
+++ pycomm.py
@@ -5,12 +5,12 @@ import ab_cip_const
 if __name__ == '__main__':
     print (ab_cip_const.EXTEND_CODES[5][1])
     c = Cip()
-    c.open('192.168.1.10')
-    # c.open('172.16.32.100')
-    v = c.read_tag('Counts')
-    print v
+    # c.open('192.168.1.10')
+    c.open('172.16.8.100')
+    v = c.read_tag('sigTriggerFaulted_3')
+    print(v)
     if v is not None:
-        c.write_tag('Counts', 23, 'DINT')
+        c.write_tag('sigTriggerFaulted_3', False, 'BOOL')
     c.close()
     logging.shutdown()
 
