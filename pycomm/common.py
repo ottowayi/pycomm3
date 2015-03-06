3a476c90063e285143321a609e60a4791bf10a73
Agostino <ruscito@gmail.com>
2015-03-06 00:34:18 -0600

    Cleanup

--- pycomm/common.py
+++ pycomm/common.py
@@ -1,3 +1,3 @@
 __author__ = 'Agostino Ruscito'
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __date__ = "01 01 2015"
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
+from pycomm import common
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 setup(
     name="pycomm",
     author="Agostino Ruscito",
     author_email="uscito@gmail.com",
-    version='0.0.2',
+    version=common.__version__,
     description="A PLC communication library for Python",
     long_description=long_description,
     license="MIT",
