7e537966f425c7bceacce148f9693a9a0ceea751
Agostino <ruscito@gmail.com>
2015-03-06 00:15:24 -0600

    Cleanup

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-
-from pycomm import common
 from distutils.core import setup
+from os import path
 
-with open('README.rst') as f:
-    description = f.read()
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="pycomm",
     author="Agostino Ruscito",
     author_email="uscito@gmail.com",
-    version=common.__version__,
+    version='0.0.2',
     description="A PLC communication library for Python",
-    long_description=description,
+    long_description=long_description,
     license="MIT",
     url="https://github.com/ruscito/pycomm",
     packages=[
