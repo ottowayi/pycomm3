192acd35e9b2b4d771fd6dc5b82ec9bf0745fa3c
ottowayi <ian@ottoway.dev>
2020-07-16 15:43:51 -0400

    version bump

--- pycomm3/__init__.py
+++ pycomm3/__init__.py
@@ -24,7 +24,7 @@
 # SOFTWARE.
 #
 
-__version_info__ = (0, 8, 0, 'dev1')
+__version_info__ = (0, 8, 0)
 __version__ = '.'.join(f'{x}' for x in __version_info__)
 
 from typing import NamedTuple, Any, Optional
