8fb0763ac8531e25769f2f05c67e01850d7695ea
Anthony Beijes <anthony.beijes@accenture.com>
2026-04-27 13:34:08 -0400

    Added docstring for new utility function

--- pycomm3/util.py
+++ pycomm3/util.py
@@ -31,6 +31,14 @@ from typing import Tuple
 
 
 def tag_lookup_key(tag_name: str) -> str:
+    """Strip array portions of the tag, and ensure lower case strings for consistent lookups
+
+    Args:
+        tag_name (str): Tag name to create key from
+
+    Returns:
+        str: Tag lookup key for the given tag name
+    """
     return strip_array(tag_name).lower()
 
 
